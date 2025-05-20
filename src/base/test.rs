ec4e6f7aae5f619c1ddd2abcb25c524cb1255c56
Victor Borja <vborja@apache.org>
2025-05-20 02:03:08 -0600

    remove type annotations

--- src/base/test.rs
+++ src/base/test.rs
@@ -2,30 +2,30 @@ use crate::Fx;
 
 #[test]
 fn test_apply_ctx_fn() {
-    let e = Fx::<&str, usize>::apply("hello");
+    let e = Fx::apply("hello");
     let v = e.provide(|s: &str| s.len()).eval();
     assert_eq!(v, Some("hello".len()))
 }
 
 #[test]
 fn test_suspend() {
-    let e = Fx::<&str, usize>::suspend("hello");
+    let e = Fx::suspend("hello");
     let v = e.provide_left(|s: &str| Fx::pure(s.len())).eval();
     assert_eq!(v, Some("hello".len()))
 }
 
 #[test]
 fn test_handler() {
-    let e = Fx::<&str, usize>::suspend("hello");
-    let handler = Fx::<&str, usize>::handler(|s: &str| Fx::pure(s.len()));
+    let e = Fx::suspend("hello");
+    let handler = Fx::handler(|s: &str| Fx::pure(s.len()));
     let v = handler.handle(e).eval();
     assert_eq!(v, Some("hello".len()))
 }
 
 #[test]
 fn test_handle() {
-    let e = Fx::<&str, usize>::handle("hello");
-    let handler = Fx::<&str, usize>::handler(|s: &str| Fx::pure(s.len()));
+    let e = Fx::handle("hello");
+    let handler = Fx::handler(|s: &str| Fx::pure(s.len()));
     let v = e.provide_left(handler).eval();
     assert_eq!(v, Some("hello".len()))
 }
