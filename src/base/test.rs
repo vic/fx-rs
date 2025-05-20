5a960b26c407b13f2e7f1c7f9c480b99f7391882
Victor Borja <vborja@apache.org>
2025-05-20 00:27:31 -0600

    test suspend

--- src/base/test.rs
+++ src/base/test.rs
@@ -6,3 +6,10 @@ fn ctx_fn_apply() {
     let v = e.provide(|s: &str| s.len()).eval();
     assert_eq!(v, Some("hello".len()))
 }
+
+#[test]
+fn test_suspend() {
+    let e = Fx::<&str, usize>::suspend("hello");
+    let v = e.provide_left(|s: &str| Fx::immediate(s.len())).eval();
+    assert_eq!(v, Some("hello".len()))
+}
