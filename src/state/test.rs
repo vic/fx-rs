97a2c329e162a37ae4045ccb853ea10d68fa0f9a
Victor Borja <vborja@apache.org>
2025-05-21 02:34:35 -0600

    test write

--- src/state/test.rs
+++ src/state/test.rs
@@ -1,4 +1,4 @@
-use crate::state::{Read, State};
+use crate::state::{Read, State, Write};
 
 #[test]
 fn handle_read() {
@@ -10,4 +10,16 @@ fn handle_read() {
     let v = e.handle_left(reader).and_nil().handle_left(state).eval();
 
     assert_eq!(v, Some(("10".to_owned(), 10)));
+}
+
+#[test]
+fn handle_write() {
+    let e = Write::write(99);
+
+    let state = State::<usize>::handler(10);
+    let writer = Write::<usize>::writer();
+
+    let v = e.handle_left(writer).and_nil().handle_left(state).eval();
+
+    assert_eq!(v, Some(((), 99)));
 }
