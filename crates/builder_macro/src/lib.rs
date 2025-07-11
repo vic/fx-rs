51c3b8b1ce6201239f480be806d07a16ee92e750
Victor Borja <vborja@apache.org>
2025-07-11 03:16:31 -0600

    feat(builder-macro): conditional Has<T> impl for builder types
    - Macro now generates impl Has<T> for builder only when the corresponding field is Present
    - Added integration test to verify Has<T> is only implemented when field is present
    - Refactored builder macro to use UpperCamelCase marker types
    - All builder macro tests pass and code is formatted
    
    Unlocks type-driven context provision and safer builder usage. Future: extend to more traits and ergonomic context composition. ��

---
+++ crates/builder_macro/src/lib.rs
@@ -0,0 +1,166 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{Data, DeriveInput, Fields, parse_macro_input};
+
+#[proc_macro_derive(ContextBuilder)]
+pub fn context_builder(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_name = &input.ident;
+    let builder_name = format_ident!("{}Builder", struct_name);
+    let mut field_idents = Vec::new();
+    let mut field_types = Vec::new();
+    if let Data::Struct(data_struct) = &input.data {
+        if let Fields::Named(fields_named) = &data_struct.fields {
+            for field in &fields_named.named {
+                field_idents.push(field.ident.as_ref().unwrap());
+                field_types.push(&field.ty);
+            }
+        }
+    }
+    // Marker types for each field (UpperCamelCase)
+    let marker_types: Vec<_> = field_idents
+        .iter()
+        .map(|id| {
+            let name = id.to_string();
+            let mut chars = name.chars();
+            let first = chars.next().unwrap().to_uppercase().collect::<String>();
+            let rest = chars.collect::<String>();
+            format_ident!("{}State", format!("{}{}", first, rest))
+        })
+        .collect();
+    // Derive Clone for marker types
+    let marker_structs = marker_types
+        .iter()
+        .map(|ty| quote! { #[derive(Clone)] pub struct #ty; });
+    let absent_generics = vec![quote! { Absent }; marker_types.len()];
+    let present_generics = vec![quote! { Present }; marker_types.len()];
+    // Derive Clone for Absent/Present
+    let marker_absent_present = quote! {
+        #[derive(Clone)]
+        pub struct Absent;
+        #[derive(Clone)]
+        pub struct Present;
+    };
+
+    // Generate unique builder field names
+    let builder_field_idents: Vec<_> = field_idents
+        .iter()
+        .map(|id| format_ident!("maybe_{}", id))
+        .collect();
+
+    // Derive Clone for all builder types
+    let builder_struct = quote! {
+        #[derive(Clone)]
+        pub struct #builder_name<#(#marker_types),*> {
+            #(#builder_field_idents: Option<#field_types>,)*
+            _marker: std::marker::PhantomData<(#(#marker_types),*)>,
+        }
+    };
+
+    // empty() for all Absent
+    let empty_impl = quote! {
+        impl #builder_name<#(#absent_generics),*> {
+            pub fn empty() -> Self {
+                Self {
+                    #(#builder_field_idents: None,)*
+                    _marker: std::marker::PhantomData,
+                }
+            }
+        }
+    };
+
+    // put_x for each field
+    let mut put_methods = Vec::new();
+    for (i, (id, ty)) in field_idents.iter().zip(field_types.iter()).enumerate() {
+        let mut generics = marker_types.clone();
+        let mut next_generics = generics.clone();
+        next_generics[i] = format_ident!("Present");
+        let assignments = builder_field_idents.iter().enumerate().map(|(j, f)| {
+            if i == j {
+                quote! { #f: Some(value) }
+            } else {
+                quote! { #f: self.#f }
+            }
+        });
+        let put_method = quote! {
+            impl<#(#generics),*> #builder_name<#(#generics),*> {
+                pub fn #id(self, value: #ty) -> #builder_name<#(#next_generics),*> {
+                    #builder_name {
+                        #(#assignments,)*
+                        _marker: std::marker::PhantomData,
+                    }
+                }
+            }
+        };
+        put_methods.push(put_method);
+    }
+
+    // build() for all Present
+    let build_impl = quote! {
+        impl #builder_name<#(#present_generics),*> {
+            pub fn build(self) -> #struct_name {
+                #struct_name {
+                    #(
+                        #field_idents: self.#builder_field_idents.unwrap(),
+                    )*
+                }
+            }
+        }
+    };
+
+    // Accessor methods
+    let mut accessor_methods = Vec::new();
+    for idx in 0..field_idents.len() {
+        let id = &field_idents[idx];
+        let ty = &field_types[idx];
+        let builder_id = &builder_field_idents[idx];
+        let get_method_name = format_ident!("maybe_{}", id);
+        let get_method = quote! {
+            pub fn #get_method_name(&self) -> Option<#ty> {
+                self.#builder_id.as_ref().map(|v| v.clone())
+            }
+        };
+        let has_method_name = format_ident!("has_builder_{}", id);
+        let has_method = quote! {
+            pub fn #has_method_name(&self) -> bool {
+                self.#builder_id.is_some()
+            }
+        };
+        accessor_methods.push(get_method);
+        accessor_methods.push(has_method);
+    }
+
+    // Conditional Has impls for builder
+    let mut has_impls = Vec::new();
+    for (i, ty) in field_types.iter().enumerate() {
+        let mut generics = marker_types
+            .iter()
+            .map(|_| quote! { Absent })
+            .collect::<Vec<_>>();
+        generics[i] = quote! { Present };
+        let builder_ty = quote! { #builder_name<#(#generics),*> };
+        let builder_field = &builder_field_idents[i];
+        has_impls.push(quote! {
+            impl Has<#ty> for #builder_ty {
+                fn get(self) -> #ty {
+                    self.#builder_field.expect("Field must be present")
+                }
+            }
+        });
+    }
+
+    let expanded = quote! {
+        #marker_absent_present
+        #(#marker_structs)*
+        #builder_struct
+        impl<#(#marker_types),*> #builder_name<#(#marker_types),*> {
+            #(#accessor_methods)*
+        }
+        #empty_impl
+        #(#put_methods)*
+        #build_impl
+        #(#has_impls)*
+    };
+    TokenStream::from(expanded)
+}
---
+++ crates/integration/tests/builder_macro_test.rs
@@ -0,0 +1,128 @@
+use builder_macro::ContextBuilder;
+use fx::Has;
+use fx_field::HasFields;
+
+#[derive(HasFields, ContextBuilder, Debug, PartialEq, Clone)]
+struct Foo {
+    a: usize,
+    b: String,
+    c: bool,
+}
+
+#[test]
+fn all_fields() {
+    let builder = FooBuilder::empty().a(42).b("hello".to_owned()).c(true);
+    let foo = builder.build();
+    assert_eq!(
+        foo,
+        Foo {
+            a: 42,
+            b: "hello".to_owned(),
+            c: true
+        }
+    );
+}
+
+#[test]
+fn partial_fields() {
+    let builder = FooBuilder::empty().a(1);
+    // Should not be able to call build() yet (type error if uncommented)
+    // let foo = builder.build();
+    assert_eq!(builder.maybe_a(), Some(1));
+    assert_eq!(builder.maybe_b(), None);
+    assert_eq!(builder.maybe_c(), None);
+}
+
+#[test]
+fn fields_any_order() {
+    let builder = FooBuilder::<Absent, Absent, Absent>::empty()
+        .c(true)
+        .b("world".to_owned())
+        .a(99);
+    let foo = builder.build();
+    assert_eq!(
+        foo,
+        Foo {
+            a: 99,
+            b: "world".to_owned(),
+            c: true
+        }
+    );
+}
+
+#[test]
+fn double_put_overwrites() {
+    let builder = FooBuilder::<Absent, Absent, Absent>::empty()
+        .a(1)
+        .a(2)
+        .b("x".to_owned())
+        .c(false);
+    let foo = builder.build();
+    assert_eq!(
+        foo,
+        Foo {
+            a: 2,
+            b: "x".to_owned(),
+            c: false
+        }
+    );
+}
+
+#[test]
+fn double_put_all_fields() {
+    let builder = FooBuilder::<Absent, Absent, Absent>::empty()
+        .a(1)
+        .a(2)
+        .b("x".to_owned())
+        .b("y".to_owned())
+        .c(false)
+        .c(true);
+    let foo = builder.build();
+    assert_eq!(
+        foo,
+        Foo {
+            a: 2,
+            b: "y".to_owned(),
+            c: true
+        }
+    );
+}
+
+#[test]
+fn all_fields_none() {
+    let builder = FooBuilder::<Absent, Absent, Absent>::empty();
+    assert_eq!(builder.maybe_a(), None);
+    assert_eq!(builder.maybe_b(), None);
+    assert_eq!(builder.maybe_c(), None);
+}
+
+#[test]
+fn all_fields_default() {
+    let builder = FooBuilder::<Absent, Absent, Absent>::empty()
+        .a(Default::default())
+        .b(Default::default())
+        .c(Default::default());
+    let foo = builder.build();
+    assert_eq!(
+        foo,
+        Foo {
+            a: 0,
+            b: String::new(),
+            c: false
+        }
+    );
+}
+
+#[test]
+fn has_trait_impl_only_when_field_present() {
+    // Builder with field 'a' present
+    let builder_start = FooBuilder::<Absent, Absent, Absent>::empty();
+    let builder = builder_start.a(1); // Now FooBuilder<Present, Absent, Absent>
+    fn assert_has_a<T: Has<usize>>(_t: &T) {}
+    assert_has_a(&builder); // Should compile
+
+    // Builder with field 'a' absent
+    let builder_absent = FooBuilder::<Absent, Absent, Absent>::empty();
+    // The following line should fail to compile if uncommented:
+    // assert_has_a(&builder_absent);
+}
