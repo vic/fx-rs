3cc93e77631941d60ea4e344cb4023081fff61b5
Victor Borja <vborja@apache.org>
2025-07-10 23:53:52 -0600

    Macro for Ability families. (#17)

---
+++ crates/abilities_macro/src/lib.rs
@@ -0,0 +1,71 @@
+// Remove duplicate imports and fix macro import
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{FnArg, Item, ReturnType, TraitItem, parse_macro_input};
+
+#[proc_macro]
+pub fn abilities(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as syn::File);
+    let mut output = quote! {};
+    for item in input.items {
+        if let Item::Trait(trait_item) = item {
+            let trait_ident = &trait_item.ident;
+            output.extend(quote! {
+                #[derive(Clone)]
+                pub struct #trait_ident;
+            });
+            for trait_item in &trait_item.items {
+                if let TraitItem::Fn(method) = trait_item {
+                    let method_ident = &method.sig.ident;
+                    let ability_fn_ident =
+                        syn::Ident::new(&format!("{}_ability", method_ident), method_ident.span());
+                    let (ctx_ty, input_ty, ret_ty) = match extract_types(&method.sig) {
+                        Some(t) => t,
+                        None => continue,
+                    };
+                    // Associated function for ability construction
+                    output.extend(quote! {
+                        impl #trait_ident {
+                            pub fn #ability_fn_ident<'f, F>(f: F) -> Box<dyn Ability<'f, #input_ty, #ctx_ty, #ret_ty> + 'f>
+                            where F: FnOnce(#input_ty) -> Fx<'f, #ctx_ty, #ret_ty> + Clone + 'f {
+                                Box::new(f)
+                            }
+                            pub fn #method_ident<'f, P>(arg: #input_ty) -> Fx<'f, P, #ret_ty>
+                            where P: Pair<Box<dyn Ability<'f, #input_ty, #ctx_ty, #ret_ty> + 'f>, #ctx_ty> {
+                                Abilities::request::<P, Box<dyn Ability<'f, #input_ty, #ctx_ty, #ret_ty> + 'f>>(arg)
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+    output.into()
+}
+
+fn extract_types(
+    sig: &syn::Signature,
+) -> Option<(
+    proc_macro2::TokenStream,
+    proc_macro2::TokenStream,
+    proc_macro2::TokenStream,
+)> {
+    let mut args = sig.inputs.iter();
+    let ret_ty = match &sig.output {
+        ReturnType::Type(_, ty) => quote! { #ty },
+        ReturnType::Default => quote! { () },
+    };
+    match (args.next(), args.next()) {
+        (Some(FnArg::Typed(arg1)), Some(FnArg::Typed(arg2))) => {
+            let ctx = &arg1.ty;
+            let input = &arg2.ty;
+            Some((quote! { #ctx }, quote! { #input }, ret_ty))
+        }
+        (Some(FnArg::Typed(arg)), None) => {
+            let input = &arg.ty;
+            Some((quote! { () }, quote! { #input }, ret_ty))
+        }
+        _ => None,
+    }
+}
---
+++ crates/integration/tests/abilities_macro_test.rs
@@ -0,0 +1,62 @@
+use abilities_macro::abilities;
+use fx::{Abilities, Ability, Fx, Has, Pair};
+
+abilities! {
+  pub trait Foo {
+    fn length(line: String) -> usize;
+  }
+
+  trait Bar {
+    fn increment(n: usize) -> usize;
+    fn decrement(n: usize) -> usize;
+  }
+}
+
+abilities! {
+  trait Baz {
+    fn len_add(ctx: String, n: u8) -> usize;
+  }
+}
+
+#[test]
+fn check_types() {
+    let _: Foo = Foo;
+    let _: Bar = Bar;
+}
+
+#[test]
+fn ability_from_closure() {
+    let _: Box<dyn Ability<'_, String, (), usize>> =
+        Foo::length_ability(|line: String| Fx::value(line.len()));
+    let _: Box<dyn Ability<'_, String, (), usize>> = Box::new(|line: String| Fx::value(line.len()));
+}
+
+#[test]
+fn check_type_of_ability_requests() {
+    fn check_type<'f, P>(_: Fx<'f, P, usize>)
+    where
+        P: Pair<Box<dyn Ability<'f, String, (), usize> + 'f>, ()>,
+    {
+    }
+    check_type(Abilities::request::<(_, _), _>("hello".to_owned()));
+    check_type(Foo::length::<(_, _)>("Hello".to_owned()));
+}
+
+#[test]
+fn ctx_ability_from_closure() {
+    let _: Box<dyn Ability<'_, u8, String, usize>> =
+        Baz::len_add_ability(|n: u8| Fx::func(move |s: String| s.len() + (n as usize)));
+    let _: Box<dyn Ability<'_, u8, String, usize>> =
+        Box::new(|n: u8| Fx::func(move |s: String| s.len() + (n as usize)));
+}
+
+#[test]
+fn ctx_check_type_of_ability_requests() {
+    fn check_type<'f, P>(_: Fx<'f, P, usize>)
+    where
+        P: Pair<Box<dyn Ability<'f, u8, String, usize> + 'f>, String>,
+    {
+    }
+    check_type(Abilities::request::<(_, _), _>(10u8));
+    check_type(Baz::len_add::<(_, _)>(10u8));
+}
